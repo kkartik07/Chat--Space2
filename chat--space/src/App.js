2b0dadfb4a55f30c346b8c940113d3a368966a8c
Kartik Kankurte <87076033+kkartik07@users.noreply.github.com>
2022-06-05 12:04:57 +0530

    Update App.js

--- chat--space/src/App.js
+++ chat--space/src/App.js
@@ -11,7 +11,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
       <Router>
         <Routes>
           <Route path='/' element={[<Join />]} />
