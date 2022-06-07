0e062161152af7fe3b1ff94e2f04301df27a30b0
Kartik Kankurte <87076033+kkartik07@users.noreply.github.com>
2022-06-08 02:39:21 +0530

    Add files via upload

---
+++ chat--space/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../stylesheets/NotFound.css'
+
+function NotFound() {
+    return (
+        <div className='not-found'>
+            <div className='card'>
+                <p>Oh No! 404 Error</p>
+                <p>Page no found :( </p>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
