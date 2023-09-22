49c05cc01baf3e3f1eccb88d7a64276d36999d24
Kartik Kankurte <kartikkankurte@gmail.com>
2023-09-23 01:16:57 +0530

    setup

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = 'http://127.0.0.1:5000';
 
 let socket;
 
